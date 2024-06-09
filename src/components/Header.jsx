caf42f1905b95b504f53b84aa6a76b1d97bf43c0
TAARUPA5 <panat.ruangsri@swisscom.com>
2024-06-09 22:11:19 +0200

    header for erik

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,43 +1,69 @@
+import { useNavigate } from "react-router-dom";
+
+
 function Header() {
+const navigate = useNavigate();
+
+const toHome = () => {
+  // route to Home
+  navigate("/");
+};
+
+const toMap = () => {
+  // route to Map
+  navigate("/map");
+};
+
+const toProfil = () => {
+  // route to Profil
+  navigate("/profil");
+};
+
+const toGame = () => {
+  // route to Games
+  navigate("/game");
+};
+
+const toShop = () => {
+  // route to Shop
+  navigate("/shop");
+};
+
   return (
   <>
-  <nav className="navbar bg-body-tertiary fixed-top">
+  <nav className="navbar text-bg-secondary fixed-top">
   <div className="container-fluid">
-    <a className="navbar-brand" href="#">Offcanvas navbar</a>
+    <div  className="col-10"><img src="/public/assets/icons/logo.png" width="120"></img></div>
     <button className="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
     <div className="offcanvas offcanvas-end" tabIndex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
       <div className="offcanvas-header">
-        <h5 className="offcanvas-title" id="offcanvasNavbarLabel">Offcanvas</h5>
+        <h5 className="offcanvas-title" id="offcanvasNavbarLabel">CINET</h5>
         <button type="button" className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
       </div>
       <div className="offcanvas-body">
         <ul className="navbar-nav justify-content-end flex-grow-1 pe-3">
           <li className="nav-item">
-            <a className="nav-link active" aria-current="page" href="#">Home</a>
+            <a className="nav-link active" aria-current="page" onClick={toHome}>Home</a>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="#">Link</a>
+            <a className="nav-link" onClick={toMap}>Map</a>
           </li>
           <li className="nav-item dropdown">
             <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-              Dropdown
+              Profil
             </a>
             <ul className="dropdown-menu">
-              <li><a className="dropdown-item" href="#">Action</a></li>
-              <li><a className="dropdown-item" href="#">Another action</a></li>
+              <li><a className="dropdown-item" onClick={toGame}>Games</a></li>
+              <li><a className="dropdown-item" onClick={toShop}>Shop</a></li>
               <li>
                 <hr className="dropdown-divider"/>
               </li>
-              <li><a className="dropdown-item" href="#">Something else here</a></li>
+              <li><a className="dropdown-item" onClick={toProfil}>View Profil</a></li>
             </ul>
           </li>
         </ul>
-        <form className="d-flex mt-3" role="search">
-          <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-          <button className="btn btn-outline-success" type="submit">Search</button>
-        </form>
       </div>
     </div>
   </div>
