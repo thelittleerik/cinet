23806e41d2f84ca5e0e84bd2a90baa38e8c9167b
TAARUPA5 <panat.ruangsri@swisscom.com>
2024-06-09 23:13:27 +0200

    timos changes

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,4 +1,19 @@
-function Card() {
-  return <>Card</>
+import React from 'react';
+import Button from './Button';
+
+function Card({ title, src, alt, description, onButtonClick }) {
+  return (
+    <div className="card mb-4 shadow-sm" style={{ borderRadius: '20px', overflow: 'hidden' }}>
+      <img src={src} className="card-img-top" alt={alt} style={{ height: '200px', objectFit: 'cover', borderTopLeftRadius: '20px', borderTopRightRadius: '20px' }} />
+      <div className="card-body">
+        <h5 className="card-title">{title}</h5>
+        <p className="card-text">{description}</p>
+        <div className="d-grid">
+          <Button className="gamebutton"  onClick={onButtonClick} style="width: 100%;">Spielen</Button>
+        </div>
+      </div>
+    </div>
+  );
 }
-export default Card;
+
+export default Card;
--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -1,4 +1,44 @@
+import React from 'react';
+import Card from '../components/Card';
+
 function Game() {
-  return <>Game</>;
+  const handleButtonClick = () => {
+    alert('Spiel gestartet!');
+  };
+
+  return (
+    <div className="container mt-4">
+      <div className="row justify-content-center">
+        <div className="col-md-4">
+          <Card
+            title="Cinema Run"
+            src="public\assets\icons\tempellrun.png"
+            alt="First Card Image"
+            description="This is the description for Game 1."
+            onButtonClick={handleButtonClick}
+          />
+        </div>
+        <div className="col-md-4">
+          <Card
+            title="Flappy Cinema"
+            src="public\assets\icons\flappybird.png"
+            alt="Second Card Image"
+            description="This is the description for Game 2."
+            onButtonClick={handleButtonClick}
+          />
+        </div>
+        <div className="col-md-4">
+          <Card
+            title="Cinema Surfer"
+            src="public\assets\icons\subwaysurfer.png"
+            alt="Third Card Image"
+            description="This is the description for Game 3."
+            onButtonClick={handleButtonClick}
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
-export default Game;
+
+export default Game;
