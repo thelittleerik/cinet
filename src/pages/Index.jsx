a9e758e2b2c37bbfa8796f2a9bb916a071e8e4ab
TAARUPA5 <panat.ruangsri@swisscom.com>
2024-06-09 22:08:57 +0200

    Revert "Push for erik"
    This reverts commit 7e1a72cfeceb1ca8b960564079d42c6b734c39fa.

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,12 +1,4 @@
-import React from "react";
 function Index() {
-  return(
-    <>
-    <video playsinline autoplay muted loop id="bgvid">
-      <source src="public/assets/icons/bg-video.mp4" type="video/mp4"/>
-    </video>
-      <h1>Home</h1>
-    </>
-  ) ;
+  return <>Index</>;
 }
 export default Index;
