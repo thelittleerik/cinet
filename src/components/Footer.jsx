4ef1c59e84ddadab574afa5a8070e7798568329d
TAARUPA5 <panat.ruangsri@swisscom.com>
2024-06-09 23:32:20 +0200

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ function Footer() {
   return (
     <>
     <div className="container">
-      <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top fixed-bottom">
+      <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top absolut-bottom">
         <div className="col-md-4 d-flex align-items-center">
           <a href="/" className="mb-3 me-2 mb-md-0 text-body-secondary text-decoration-none lh-1">
             <svg className="bi" width="30" height="24"><use xlink:href="#bootstrap"></use></svg>
